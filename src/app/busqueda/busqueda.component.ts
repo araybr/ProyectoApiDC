529c1616359a06863d1fcb405aea7736b6195a37
abritor <araybrito2005@gmail.com>
2025-02-12 21:02:42 +0100

    "Busqueda por nombre e ids"

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -9,37 +9,95 @@ import {Character} from '../../character.model';
   styleUrl: './busqueda.component.css'
 })
 export class BusquedaComponent {
-  characters = [];
+  characters:Character[] = [];
+  characterBusqueda1 = []
+  characterBusqueda2 = []
+  esBusquedaNombre = false;
+  esPagina2 = false
+  pagina = 1;
   inicio = 1;
   final = 10;
-  async buscar(){
-    var url = 'https://rickandmortyapi.com/api/character/';
-    for (let i = this.inicio; i <= this.final; i++) {
-      if(i<=826){
-        url+=i.toString()
-        if(i!=this.final){
-          url+=","
+  busqueda = "";
+  async buscar(busqueda:string){
+    this.busqueda = busqueda;
+    if(this.busqueda == ""){
+      this.pagina = 1;
+      this.esBusquedaNombre = false;
+      var url = 'https://rickandmortyapi.com/api/character/';
+      for (let i = this.inicio; i <= this.final; i++) {
+        if(i<=826){
+          url+=i.toString()
+          if(i!=this.final){
+            url+=","
+          }
+        }else{
+          break;
         }
-      }else{
-        break;
       }
+      const response = await fetch(url);
+      const data = await response.json();
+      this.characters = data.map((c: any) => new Character(c));
+    }else if(isNaN(this.busqueda as any)){
+      this.esBusquedaNombre = true;
+      var url = 'https://rickandmortyapi.com/api/character/?page=' + this.pagina.toString() + '&name=' + this.busqueda;
+      const response = await fetch(url);
+      const data = await response.json();
+      this.characterBusqueda1 = data["results"].slice(0,10)
+      this.characterBusqueda2 = data["results"].slice(10)
+      if (!this.esPagina2){
+        this.characters = this.characterBusqueda1.map((c: any) => new Character(c))
+      }else{
+        this.characters = this.characterBusqueda2.map((c: any) => new Character(c))
+      }
+    }else{
+      this.pagina = 1
+      var url = 'https://rickandmortyapi.com/api/character/' + this.busqueda;
+      const response = await fetch(url);
+      const data = await response.json();
+      this.characters.push(new Character(data))
     }
-    const response = await fetch(url);
-    const data = await response.json();
-    this.characters = data.map((c: any) => new Character(c));
   }
   siguientePagina(){
-    if (this.final<=830){
-      this.inicio+=10;
-      this.final+=10;
+    if (this.esBusquedaNombre){
+      this.inicio = 1;    //Reseteo la busqueda normal
+      this.final = 10;
+      if(this.esPagina2){
+          this.pagina +=1;
+          this.esPagina2 = false;
+      }else{
+        this.esPagina2 = true;
+      }
+    }else{
+      this.pagina = 1;    //Reseteo la busqueda por Nombre
+      if (this.final<=830){
+        this.inicio+=10;
+        this.final+=10;
+      }
     }
-    this.buscar();
+    this.buscar(this.busqueda);
   }
   anteriorPagina(){
-    if(this.inicio>1){
-      this.inicio-=10;
-      this.final-=10;
+    if (this.esBusquedaNombre){
+      this.inicio = 1;   //Reseteo la busqueda normal
+      this.final = 10;
+      if(!this.esPagina2){
+        if (this.pagina>1){
+          this.pagina -= 1;
+          this.esPagina2 = true;
+        }
+      }else{
+        this.esPagina2 = false;
+      }
+    }else{
+      this.pagina = 1;   //Reseteo la busqueda por Nombre
+      if(this.inicio>1){
+        this.inicio-=10;
+        this.final-=10;
+      }
     }
-    this.buscar();
+    this.buscar(this.busqueda);
+  }
+  buscarPorGenero(genero:string){
+
   }
 }
